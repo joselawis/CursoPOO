3b659fd3d03cba47aa646be22fb5945287042b83
Joseluis Contreras Martinez <joselawis@gmail.com>
2019-12-05 21:18:08 +0100

    Toda la tarde jugando con metodos extendidos, lambdas y linq

---
+++ Modulo5/Modulo5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vector1 = new Vector(new int[] { 1, 4 });
+            var vector2 = new Vector(new int[] { 2, 5 });
+            var vector3 = new Vector(new int[] { 1, 2, 3 });
+
+            // Sumamos los vectores y los pintamos
+            Vector vectorSuma = vector1.SumarVector(vector2);
+            Console.WriteLine($"[{vectorSuma[0]},{vectorSuma[1]}]");
+
+            // Metodo PrintVector para pintar los vectores
+            vectorSuma.PrintVector();
+
+            // Sumamos con el operador
+            (vector1 + vector2).PrintVector();
+            new Vector[] { vector1, vector2, vectorSuma }.SumarVectores().PrintVector();
+
+            new Vector(3).PrintVector();
+
+            Vector vectorSumaError = (vector1 + vector3); // --> Salta Excepcion
+        }
+    }
+
+    public class Vector
+    {
+        private int[] Valores { get; set; }
+
+        public int Dimension { get { return Valores.Length; } }
+
+        public Vector(int[] valores)
+        {
+            Valores = valores;
+        }
+
+        public Vector(int dimension)
+        {
+            Valores = new int[dimension].DefaultIfEmpty(0).ToArray();
+        }
+
+        public int this[int i] {
+            get { return Valores[i]; }
+            set { Valores[i] = value; }
+        }
+
+        public IEnumerable<int> ToEnumerable()
+        {
+            return Valores.AsEnumerable();
+        }
+
+        public static Vector operator + (Vector v1, Vector v2)
+        {
+            return v1.SumarVector(v2);
+        }
+    }
+}
---
+++ Modulo5/Modulo5/VectorUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo5
+{
+    public static class VectorUtils
+    {
+        // Funcion binaria IN <int,int> => OUT <int>
+        private static Func<int, int, int> Addition = (i1, i2) => i1 + i2;
+        private static Func<int, int, int> Substraction = (i1, i2) => i1 - i2;
+
+        // Suma 2 vectores
+        public static Vector SumarVector(this Vector v1, Vector v2)
+        {
+            if (v1.Dimension != v2.Dimension) throw new ApplicationException("No puedes sumar vectores de distintas dimensiones");
+
+            return new Vector(v1.ToEnumerable().DefaultIfEmpty(0).Zip(v2.ToEnumerable().DefaultIfEmpty(0), Addition).ToArray());
+        }
+
+        // Suma una lista de vectores
+        public static Vector SumarVectores(this IEnumerable<Vector> vectors)
+        {
+            return vectors.Aggregate(new Vector(vectors.First().Dimension), (acc, next) => next.SumarVector(acc));
+        }
+
+        // Pinta por consola un vector en formato [i1, ... ,iN]
+        public static void PrintVector(this Vector vector)
+        {
+            Console.WriteLine($"[{vector.ToEnumerable().DefaultIfEmpty(0).First()}{vector.ToEnumerable().DefaultIfEmpty(0).Skip(1).Aggregate<int, string>(String.Empty, (acc, i) => acc + "," + i)}]");
+        }
+    }
+}
