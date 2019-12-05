d2df8953c1932f64bca3c48ed584f89c652297c6
Joseluis Contreras Martinez <joselawis@gmail.com>
2019-12-05 18:25:52 +0100

    Modificadores de acceso

---
+++ Modulo4/Libreria/Libreria.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria
+{
+    public class ClienteDentroDeLibreria
+    {
+        public void Prueba()
+        {
+            var clasePublica = new ClasePublicaDePrueba();
+            //clasePublica.MetodoPrivado(); ---> ERROR El metodo solo se puede invocar desde la propia clase
+            clasePublica.MetodoPublico();
+            clasePublica.PropiedadInterna = "INTERNAL";
+            clasePublica.PropiedadProtectedInternal = "PROINT";
+            var claseInterna = new ClaseInternaDePrueba();
+
+            //clasePublica.PropiedadPrivateProtected = 5; --> ERROR Solo se puede acceder desde la clase o sus hijas
+        }
+    }
+
+    public class ClasePublicaDePrueba
+    {
+        public int PropiedadPublica { get; set; }                           // Acceso total
+        internal string PropiedadInterna { get; set; }                      // Acceso a nivel de proyecto
+        protected int PropiedadProtegida { get; set; }                      // Acceso a nivel de clase y sus hijas (independiente de proyecto)
+        protected internal string PropiedadProtectedInternal { get; set; }  // Acceso a nivel de clase y sus hijas y para todos los miembros del proyecto
+        private protected int PropiedadPrivateProtected { get; set; }       // Acesso a nivel de clase y sus hijas solo en este proyecto
+
+        // public void MetodoRecibeClaseInterna(ClaseInternaDePrueba claseInterna) --> ERROR ya que hay inconsistencia de accesibilidad
+
+
+        public void MetodoPublico()
+        {
+            PropiedadPublica = 1;
+            PropiedadInterna = "EE!!";
+            PropiedadProtegida = 4;
+            PropiedadProtectedInternal = "b";
+            PropiedadPrivateProtected = 3;
+            MetodoPrivado();
+        }
+
+        private void MetodoPrivado()
+        {
+
+        }
+    }
+
+    public class ClaseDerivada: ClasePublicaDePrueba
+    {
+        public void Prueba()
+        {
+            PropiedadPrivateProtected = 1;
+        }
+    }
+
+    internal class ClaseInternaDePrueba
+    {
+
+    }
+}
---
+++ Modulo4/Modulo4/Program.cs
@@ -0,0 +1,32 @@
+﻿using Libreria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clasePublica = new ClasePublicaDePrueba();
+            clasePublica.MetodoPublico();
+            //clasePublica.PropiedadInterna = "INTERNAL" -- > ERROR Solo se puede acceder desde su proyecto
+            //var claseInterna = new ClaseInternaDePrueba(); --> ERROR Solo se puede acceder desde su proyecto
+            //clasePublica.PropiedadProtegida --> ERROR Solo se puede acceder desde la clase o una derivada
+            var claseDerivada = new ClaseDerivada();
+        }
+    }
+
+    public class ClaseDerivada : ClasePublicaDePrueba
+    {
+        public void Prueba()
+        {
+            PropiedadProtegida = 27;
+            PropiedadProtectedInternal = "Soy tu hija desde otro proyecto";
+            //PropiedadPrivateProtected = 1; --> ERROR No se puede acceder aun siendo derivada ya que esta en otro proyecto
+        }
+    }
+}
